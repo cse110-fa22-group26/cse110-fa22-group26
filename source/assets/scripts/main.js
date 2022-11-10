7ed939298b1623856e262ce4274685bacb1f06be
Alberto Valencia <alvalenc@ucsd.edu>
2022-11-09 19:01:58 -0800

    Updated sign up page and outline for saving users
    Co-authored-by: gdietz8 <gdietz8@users.noreply.github.com>
    Co-authored-by: Samhita Kadali <oksami@users.noreply.github.com>
    Co-authored-by: Jack Multani <JackMultani@users.noreply.github.com>

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -9,3 +9,14 @@ mondayList.addEventListener("click", (event) => {
         taskBoard.style.display = "none";
     }
 });
+
+
+/*
+1) add submit event listener for both the welcome and sign up page(on click takes back to sign in?)
+2) on click we store information to local storage (?)
+    - create array storing all user's information
+        - object within an object ?
+    - we would set item (username as key and password is value)
+3) On login, user is matched to their home page
+
+*/
