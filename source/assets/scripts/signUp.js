470ade33453ff4d6b6edb623c5de4c4262f75915
Sepehr Heravi <sepehr.heravi2008@gmail.com>
2022-11-16 20:30:48 -0800

    FIxed ending html tags and styling

--- source/assets/scripts/signUp.js
+++ source/assets/scripts/signUp.js
@@ -1,50 +1,49 @@
 window.addEventListener("DOMContentLoaded", init);
 
-function init(){
-    getUserSignUpInfo();
+function init() {
+  getUserSignUpInfo();
 }
 /**
  * This function rertrieves the information the user inputs.
  */
 function getUserSignUpInfo() {
-    // find form
-    let loginForm = document.querySelector("form");
-    // when user click submit btn, check for records in localstorage
-    loginForm.addEventListener("submit", (event) => {
-      event.preventDefault();
-      // get form data
-      const formData = new FormData(loginForm);
-      // save form data into userInput to find relevant credential
-      let userInput = {};
-      formData.forEach((value, key) => (userInput[key] = value));
+  // find form
+  let loginForm = document.querySelector("form");
+  // when user click submit btn, check for records in localstorage
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // get form data
+    const formData = new FormData(loginForm);
+    // save form data into userInput to find relevant credential
+    let userInput = {};
+    formData.forEach((value, key) => (userInput[key] = value));
 
+    console.log(userInput);
 
-      console.log(userInput);
-
-      if(userInput){
-        //object exists
-        const username = userInput.username;
-        const password = userInput.password;
-        const confirmPassword = userInput.confirmPassword;
-        if(password !== confirmPassword) {
-          const checkPass = document.querySelector(".warningMessage");
-          checkPass.textContent = "Passwords don't match!";
-          loginForm.addEventListener("keypress", (event) => { //clears warning when user types
-            checkPass.textContent = "";
-          });
-          
-          }else {
-          //handle using local storage and if passwords match, taken to list
-          console.log("Passwords match!");
-          const checkPass = document.querySelector(".warningMessage");
+    if (userInput) {
+      //object exists
+      const username = userInput.username;
+      const password = userInput.password;
+      const confirmPassword = userInput.confirmPassword;
+      if (password !== confirmPassword) {
+        const checkPass = document.querySelector(".warning-signup");
+        checkPass.textContent = "Passwords don't match!";
+        loginForm.addEventListener("keypress", (event) => {
+          //clears warning when user types
           checkPass.textContent = "";
-        }
+        });
+      } else {
+        //handle using local storage and if passwords match, taken to list
+        console.log("Passwords match!");
+        const checkPass = document.querySelector(".warning-signup");
+        checkPass.textContent = "";
+        loginForm.reset();
       }
-      loginForm.reset();
-      // check if credential exist in db. If match, get and save related taskDB
-      
-    });
-  }
+    }
+
+    // check if credential exist in db. If match, get and save related taskDB
+  });
+}
 
 /*
   we need a function to save user input to local storage
@@ -52,6 +51,3 @@ function getUserSignUpInfo() {
   ensure that username does not already exist. If it works,
   then save to local storage
 */
-
-
-
