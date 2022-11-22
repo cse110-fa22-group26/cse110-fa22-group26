46454e8b609829c5597d2390d2ab852c40e74f28
Fucheng Shang <shangfucheng1@hotmail.com>
2022-11-22 06:19:34 -0800

    added shadow dom style

--- source/assets/scripts/taskShadowDom.js
+++ source/assets/scripts/taskShadowDom.js
@@ -51,6 +51,42 @@ class taskCard extends HTMLElement {
       newTaskDiv.getElementsByTagName('button')[0].disabled=data["confirmDisable"];
       newTaskDiv.getElementsByTagName('input')[0].checked = data["checkBox"];
       shadowRoot.appendChild(newTaskDiv);
+
+      let style = document.createElement("style");
+      style.textContent = `
+        /*style for checkbox*/
+        input[type=checkbox] {
+            accent-color: rgb(78, 146, 23);
+            width: 20px;
+            height: 20px;
+            cursor: pointer;
+        }
+
+        /*style for input text box*/
+        input[type=text] {
+            background: #f4ecec;
+            width: 80%;
+            padding: 10px 10px;
+            margin: 8px;
+        }
+
+        /*style for submit btn*/
+        button[type=submit] {
+            color: #03324a;
+            padding:5px 15px; 
+            background:transparent; 
+            border:0 none;
+            cursor: pointer;
+            -webkit-border-radius: 5px;
+            border-radius: 10px; 
+            transition: all 0.15s;
+        }
+
+        button[type=submit]:hover {
+            color:#1DDDDD;
+        }
+      `;
+      shadowRoot.append(style);
     }
     
   }
