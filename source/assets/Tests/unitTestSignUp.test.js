81e649ab68e286cdf557f4ecea79eea7e72eda3d
valenciaaalberto <114364115+valenciaaalberto@users.noreply.github.com>
2022-11-26 19:30:56 -0800

    Commented out tests and title

--- source/assets/Tests/unitTestSignUp.test.js
+++ source/assets/Tests/unitTestSignUp.test.js
@@ -9,31 +9,31 @@
  * different passwords, then nothing is added to localstorage
  */
 //console.log(checkNothingInDB());
-function checkNothingInDB(){
-    //Manually populate, then check that local storage is empty
-    let loginForm = document.querySelector("form");
-    const user = document.getElementById('username').value = 'user';
-    const pass = document.getElementById('password').value = '1234';
-    const confPass = document.getElementById('confirmPassword').value = '123';
-    loginForm.click();
-    //expect(localStorage.length()).toBe(0);
-   // test('mismatching passwords', () => {
-        //expect(localStorage.length()).toBe(0);
-    //});
-    if(pass !== confPass) return localStorage.length === 0;
-}
+// function checkNothingInDB(){
+//     //Manually populate, then check that local storage is empty
+//     let loginForm = document.querySelector("form");
+//     const user = document.getElementById('username').value = 'user';
+//     const pass = document.getElementById('password').value = '1234';
+//     const confPass = document.getElementById('confirmPassword').value = '123';
+//     loginForm.click();
+//     //expect(localStorage.length()).toBe(0);
+//    // test('mismatching passwords', () => {
+//         //expect(localStorage.length()).toBe(0);
+//     //});
+//     if(pass !== confPass) return localStorage.length === 0;
+// }
 
-/**
- * This function is responsible for checking that if the user enters two
- * identical passwords, then there should be a user in local storage
- */
- console.log(validUser());
- function validUser(){
-    let loginForm = document.querySelector("form");
-    const user = document.getElementById('username').value = 'user';
-    const pass = document.getElementById('password').value = '1234';
-    const confPass = document.getElementById('confirmPassword').value = '1234';
-    loginForm.click();
-    if(pass === confPass) return localStorage.length > 0;
-}
+// /**
+//  * This function is responsible for checking that if the user enters two
+//  * identical passwords, then there should be a user in local storage
+//  */
+//  console.log(validUser());
+//  function validUser(){
+//     let loginForm = document.querySelector("form");
+//     const user = document.getElementById('username').value = 'user';
+//     const pass = document.getElementById('password').value = '1234';
+//     const confPass = document.getElementById('confirmPassword').value = '1234';
+//     loginForm.click();
+//     if(pass === confPass) return localStorage.length > 0;
+// }
 
