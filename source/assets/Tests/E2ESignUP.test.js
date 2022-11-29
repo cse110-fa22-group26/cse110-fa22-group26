a5f09923769794106a2205f704aba79e2d6d2205
w3dong <w3dong@gmail.com>
2022-11-29 13:33:50 -0800

    fix jest error

---
+++ source/assets/Tests/E2ESignUP.test.js
@@ -0,0 +1,29 @@
+describe('Test planner app sign up page', () => {
+    // visit the homepage 
+    beforeAll(async () => {
+      await page.goto('https://cse110-fa22-group26.github.io/cse110-fa22-group26/source/signUp.html');
+    });
+
+    // Check to make sure that Add button can correctly add task under all weekday list
+    it('password unmatch, no new user adds to localStorage', async () => {
+        console.log(`new user input form`);
+        await page.waitForSelector('input[name=username]');
+        await page.$eval('input[name=username]', el => el.value = 'banana');
+
+        await page.waitForSelector('input[name=password]');
+        await page.$eval('input[name=password]', el => el.value = '123');
+
+        await page.waitForSelector('input[name=confirmPassword]');
+        await page.$eval('input[name=confirmPassword]', el => el.value = '12');
+
+        await page.click('button[type="submit"]');
+        await page.waitForSelector('p.warning-signup');
+        const text = await page.evaluate(() => {
+            const anchor = document.querySelector('p.warning-signup');
+            return anchor.textContent;
+        });
+        console.log(text);
+        expect(text).toBe("Passwords don't match!")
+        
+      });
+});
