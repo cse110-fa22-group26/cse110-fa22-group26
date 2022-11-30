0c6eb762dcab7491e602694d74f7a38143748583
jmultani1 <jmultani@ucsd.edu>
2022-11-30 15:45:53 -0800

    fixed timeout error for testing

--- source/assets/Tests/E2ESignUP.test.js
+++ source/assets/Tests/E2ESignUP.test.js
@@ -1,4 +1,4 @@
-const { Browser } = require("puppeteer");
+const { Browser, default: puppeteer } = require("puppeteer");
 
 describe('Test planner app sign up page', () => {
     // visit the homepage 
@@ -7,33 +7,33 @@ describe('Test planner app sign up page', () => {
     });
 
     // Check to make sure that nothing is added to local storage if passwords don't match
-    // it('password unmatch, no new user adds to localStorage', async () => {
-    //     console.log(`new user input form`);
-    //     await page.waitForSelector('input[name=username]');
-    //     await page.$eval('input[name=username]', el => el.value = 'banana');
+     it('password unmatch, no new user adds to localStorage', async () => {
+         console.log(`new user input form`);
+         await page.waitForSelector('input[name=username]');
+         await page.$eval('input[name=username]', el => el.value = 'banana');
 
-    //     await page.waitForSelector('input[name=password]');
-    //     await page.$eval('input[name=password]', el => el.value = '123');
+         await page.waitForSelector('input[name=password]');
+         await page.$eval('input[name=password]', el => el.value = '123');
 
-    //     await page.waitForSelector('input[name=confirmPassword]');
-    //     await page.$eval('input[name=confirmPassword]', el => el.value = '12');
+         await page.waitForSelector('input[name=confirmPassword]');
+         await page.$eval('input[name=confirmPassword]', el => el.value = '12');
 
-    //     await page.click('button[type="submit"]');
-    //     await page.waitForSelector('p.warning-signup');
-    //     const text = await page.evaluate(() => {
-    //         const anchor = document.querySelector('p.warning-signup');
-    //         return anchor.textContent;
-    //     });
-    //     console.log(text);
-    //     let arr = await page.evaluate(() => {
-    //       return localStorage.getItem('username');
-    //     });
-    //     expect(arr).toBe(null);
-    //     //expect(localStorage.getItem("username")).toBe(null);
-    //     //expect(text).toBe("Passwords don't match!")
-    //     // test localStorage here
-    //     // localStorage.getItem("username") === null)        
-    //   });
+         await page.click('button[type="submit"]');
+         await page.waitForSelector('p.warning-signup');
+         const text = await page.evaluate(() => {
+             const anchor = document.querySelector('p.warning-signup');
+             return anchor.textContent;
+         });
+         console.log(text);
+         let arr = await page.evaluate(() => {
+           return localStorage.getItem('username');
+         });
+         expect(arr).toBe(null);
+         //expect(localStorage.getItem("username")).toBe(null);
+         //expect(text).toBe("Passwords don't match!")
+         // test localStorage here
+         // localStorage.getItem("username") === null)        
+       });
 
       it('password match, user added to localStorage', async () => {
         await page.reload();
@@ -48,16 +48,16 @@ describe('Test planner app sign up page', () => {
         await page.$eval('input[name=confirmPassword]', el => el.value = '123');
 
         await page.click('button[type="submit"]');
-        await page.waitForSelector('p.warning-signup');
+       /* await page.waitForSelector('p.warning-signup');
         const text = await page.evaluate(() => {
             const anchor = document.querySelector('p.warning-signup');
             return anchor.textContent;
-        });
-        let retrieveUsername = await page.evaluate(() => {
-          return localStorage.getItem('username');
-        });
-        expect(retrieveUsername).toBe('banana');
+        });*/
         
+        let retrieveUsername = await page.evaluate(() => {
+          return localStorage.getItem('user');
+        });
+        expect(retrieveUsername).toBe(`{\"username\":\"banana\",\"tasks\":[[],[],[],[],[],[],[]]}`);
       }, 10000);
       
       
