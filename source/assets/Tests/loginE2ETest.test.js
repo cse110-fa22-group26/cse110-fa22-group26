75823def10a02b3089a7ca9aa5a26d8f1c041097
valenciaaalberto <114364115+valenciaaalberto@users.noreply.github.com>
2022-12-02 14:15:24 -0800

    Updated test for correct login

--- source/assets/Tests/loginE2ETest.test.js
+++ source/assets/Tests/loginE2ETest.test.js
@@ -2,29 +2,6 @@ const { Browser, default: puppeteer } = require("puppeteer");
 
 describe('Test planner app welcome page', () => {
     beforeAll(async () => {
-        await page.evaluateOnNewDocument(function () {
-            window.localStorage.clear();
-            //save template user to local storage
-            window.localStorage.setItem(
-                "user",
-                JSON.stringify({
-                  username: "userOne",
-                  tasks: [["Do Hw 1"], ["Do Hw 2"], ["Do Hw 3"], ["Do Hw 4"],
-                  ["Do Hw 5"], ["Do Hw 6"], ["Do Hw 7"]],
-                })
-            );
-
-            window.localStorage.setItem(
-              "todoListDB",
-              JSON.stringify([
-                {
-                  username: "userOne",
-                  password: "12345",
-                  tasks: [[], [], [], [],[], [], []],
-                },
-              ])
-            );
-        });
         //TODO: must switch url before submitting
         await page.goto('http://127.0.0.1:5501/source/welcomePage.html');
     });
@@ -85,7 +62,18 @@ describe('Test planner app welcome page', () => {
 
         //After user who exists login, their information in local storage be able to be retrieved
         //from the home page
-        expect(retrieveUsername).toBe(`{\"username\":\"userOne\",\"tasks\":[["Do Hw 1"],["Do Hw 2"],["Do Hw 3"],["Do Hw 4"],["Do Hw 5"],["Do Hw 6"],["Do Hw 7"]]}`);
+        await page.waitForSelector('p.warning-username');
+        const usernameWarning = await page.evaluate(() => {
+            const anchor = document.querySelector('p.warning-username');
+            return anchor.textContent;
+        });
+        expect(usernameWarning).toBe("");
+        await page.waitForSelector('p.warning-password');
+        const passwordWarning = await page.evaluate(() => {
+            const anchor = document.querySelector('p.warning-password');
+            return anchor.textContent;
+        });
+        expect(passwordWarning).toBe("");
       }, 10000);
 
 });
