7dac528dab65c04b9466a5e378b8f780876a32aa
valenciaaalberto <114364115+valenciaaalberto@users.noreply.github.com>
2022-12-02 14:48:49 -0800

    Updated test on checking user is able to log in

--- source/assets/Tests/loginE2ETest.test.js
+++ source/assets/Tests/loginE2ETest.test.js
@@ -2,12 +2,35 @@ const { Browser, default: puppeteer } = require("puppeteer");
 
 describe('Test planner app welcome page', () => {
     beforeAll(async () => {
+        await page.evaluateOnNewDocument(function () {
+            window.localStorage.clear();
+            //save template user to local storage
+            window.localStorage.setItem(
+                "user",
+                JSON.stringify({
+                  username: "userOne",
+                  tasks: [[],[],[],[],[],[],[]],
+                })
+            );
+
+            window.localStorage.setItem(
+              "todoListDB",
+              JSON.stringify([
+                {
+                  username: "userOne",
+                  password: "12345",
+                  tasks: [[], [], [], [],[], [], []],
+                },
+              ])
+            );
+        });
         //TODO: must switch url before submitting
         await page.goto('http://127.0.0.1:5501/source/welcomePage.html');
     });
 
     //Given that user has not yet signed up, warning message must be displayed
     it('username does not exist, warning message displayed', async () => {
+        await page.reload();
         await page.waitForSelector('input[name=username]');
         await page.$eval('input[name=username]', el => el.value = 'joe');
 
@@ -21,59 +44,50 @@ describe('Test planner app welcome page', () => {
             const anchor = document.querySelector('p.warning-username');
             return anchor.textContent;
         });
-  
         expect(text).toBe("Username does not exist");
       }, 10000);
 
         //If user who exists tries to login, but their password is incorrect, message should be displayed
         it('username exist correctly with correct information in localstorage', async () => {
             //User log's in
+            await page.reload();
+            await page.waitForSelector('input[name=username]');
             await page.$eval('input[name=username]', el => el.value = 'userOne');
-    
+
             await page.waitForSelector('input[name=password]');
             await page.$eval('input[name=password]', el => el.value = '1234')
-    
+
             await page.click('button[type="submit"]');
-    
+
             //should be notified about incorrect password
             await page.waitForSelector('p.warning-password');
             const warning = await page.evaluate(() => {
                 const anchor = document.querySelector('p.warning-password');
                 return anchor.textContent;
             });
-      
+
             expect(warning).toBe("Incorrect Password");
         }, 10000);
 
-      //User who exists should be able to login. At login, their information storage to
-      //local storage should be retrievable 
-      it('username exist correctly with correct information in localstorage', async () => {
-        //Look at before all method. User is signed up
+      // If user exists, they should be redirected to homepage
+      it('user exist, so no warning is displayed', async () => {
+        await page.reload();
+        const correctPage = "http://127.0.0.1:5501/source/homePage.html";
+        await page.waitForSelector('input[name=username]');
         await page.$eval('input[name=username]', el => el.value = 'userOne');
 
         await page.waitForSelector('input[name=password]');
         await page.$eval('input[name=password]', el => el.value = '12345')
+
         await page.click('button[type="submit"]');
-        
-        //retrieve user's information
-        let retrieveUsername = await page.evaluate(() => {
-          return localStorage.getItem('user');
+
+        const currentWebPage = await page.evaluate(() => {
+            return window.location.href
         });
 
-        //After user who exists login, their information in local storage be able to be retrieved
-        //from the home page
-        await page.waitForSelector('p.warning-username');
-        const usernameWarning = await page.evaluate(() => {
-            const anchor = document.querySelector('p.warning-username');
-            return anchor.textContent;
-        });
-        expect(usernameWarning).toBe("");
-        await page.waitForSelector('p.warning-password');
-        const passwordWarning = await page.evaluate(() => {
-            const anchor = document.querySelector('p.warning-password');
-            return anchor.textContent;
-        });
-        expect(passwordWarning).toBe("");
+        expect(currentWebPage).toBe(correctPage);
+
+        
       }, 10000);
 
 });
