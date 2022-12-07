cf8888cd6eeed2ba951c5801087cfe83925437df
w3dong <96416790+w3dong@users.noreply.github.com>
2022-12-06 22:38:46 -0800

    Update jest-puppeteer.config.js

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,9 +1,22 @@
-module.exports = {
-    launch: {
-      headless: false,
-      slowMo: 25
-    },
-    env: {
-        DISPLAY: ":10.0"
-    }
+const ci = Boolean(process.env.CI || false);
+baseOptions = {
+   launch: {
+     headless: false,
+     slowMo: 25,
+   }
+ };
+const ciPipelineOptions = {
+  launch: {
+    executablePath: '/usr/bin/google-chrome-stable',
+    headless: true,
+    args: [
+      '--ignore-certificate-errors',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu'
+    ]
+  },
+server: baseOptions.server
 }
+module.exports = ci ? ciPipelineOptions : baseOptions;
