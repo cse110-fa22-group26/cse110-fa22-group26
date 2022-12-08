d17023272ee69bf695c234babcc5f1f680d4da95
w3dong <96416790+w3dong@users.noreply.github.com>
2022-12-07 19:43:52 -0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,5 @@
 const config = {
     "preset": "jest-puppeteer",
-    "collectCoverage": true,
-    "collectCoverageFrom": [
-      "source/**/*"
-    ],
-    "coverageReporters": [
-      "text",
-      "lcov",
-      "cobertura"
-    ],
-    "setupFilesAfterEnv": [
-      "jest-puppeteer-istanbul/lib/setup"
-    ],
-    "reporters": [
-      "default",
-      "jest-puppeteer-istanbul/lib/reporter"
-    ],
-    "coverageDirectory": "coverage"
+    "verbose": true
   }
 module.exports = config;
